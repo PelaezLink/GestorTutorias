3d259cfe65218ce29dd6077e2428abd6ab391bb3
Luis <Luis@DESKTOP-DCU5CT2>
2020-12-02 23:41:46 +0100

    Primer pinceladas

--- src/src/FXMLGestorTutoriasController.java
+++ src/src/FXMLGestorTutoriasController.java
@@ -5,31 +5,69 @@
  */
 package src;
 
+import com.sun.javafx.scene.control.skin.DatePickerSkin;
 import java.net.URL;
+import java.time.DayOfWeek;
+import java.time.LocalDate;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.control.DateCell;
+import javafx.scene.control.DatePicker;
 import javafx.scene.control.Label;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.paint.Color;
 
 /**
  *
  * @author Luis
  */
 public class FXMLGestorTutoriasController implements Initializable {
-    
+
     @FXML
-    private Label label;
-    
-    @FXML
-    private void handleButtonAction(ActionEvent event) {
-        System.out.println("You clicked me!");
-        label.setText("Hello World!");
-    }
-    
+    private BorderPane borderPane;
+    private DatePicker calendario;
+
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
-    }    
-    
+        inicializarCalendario();
+    }
+
+    public void inicializarCalendario() {
+        DatePicker calendario = new DatePicker(LocalDate.now());
+        calendario.setShowWeekNumbers(false);
+        calendario.setDayCellFactory(cel -> new DiaCelda());
+
+        DatePickerSkin datePickerSkin = new DatePickerSkin(calendario);
+        Node popupContent = datePickerSkin.getPopupContent();
+
+        borderPane.setCenter(popupContent);
+
+    }
+
+}
+
+class DiaCelda extends DateCell {
+
+    String newline = System.getProperty("line.separator");
+
+    @Override
+    public void updateItem(LocalDate item, boolean empty) {
+        super.updateItem(item, empty); //To change body of generated methods, choose Tools | Templates.
+
+        // Show Weekends in blue color
+        DayOfWeek day = DayOfWeek.from(item);
+        if (day == DayOfWeek.SATURDAY || day == DayOfWeek.SUNDAY) {
+            this.setTextFill(Color.ROSYBROWN);
+            this.setDisable(true);
+
+            this.setText(this.getText() + "\r");
+        } else {
+            this.setText(this.getText() + "\rlibre");
+        }
+    }
+
 }
